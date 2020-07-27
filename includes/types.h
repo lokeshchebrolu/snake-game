f3ca836216cc081aa10efb14ae75c3345607b7f9
Lokesh Chebrolu <60533078+lokeshch007@users.noreply.github.com>
2020-07-27 15:02:22 +0530

    Added comments to types.h
    Added comment for more understanding

--- includes/types.h
+++ includes/types.h
@@ -1,40 +1,85 @@
 #ifndef __TYPES_H
 #define __TYPES_H
+/* File Description: All object types of game*/
 
+/* Vertex of a 2D Plane. Has (x,y) values  */
 typedef struct vertix
 {
-	float x;
-	float y;
+	float x; /* X co-ordinate value */
+	float y; /* Y co-ordinate value */
 }vertex;
 
+/* Block is a polygon with 4 vertices
+   Example: Square, rectangle */
 typedef struct block
 {
+	/*4 Points for 4 vertices */
 	vertex point[4];
 }block;
 
+/**************************/
+/*    Snake body shape    */
+/*                        */
+/*          ___________   */
+/*         |           |  */
+/*         |    UP     |  */
+/*   ______|___________|  */
+/*  |           |         */
+/*  |   DOWN    |         */
+/*  |___________|         */
+/*                        */
+/**************************/
 typedef struct snake_body
 {
-	block up;
-	block down;
-	int number;
-	int direction;
-	int rotate;
+	block up; /* UP block */
+	block down; /* DOWN block */
+	int number; /* position in whole body */
+	int direction; /* Movement direction */
+	int rotate; /* Rotation status 1: to be rotated; 0: no rotation needed */
 }snake_body;
 
+
+/**************************/
+/*    Snake Head shape    */
+/*                        */
+/*     _______            */
+/*    |       |           */
+/*    |       |_______    */
+/*    |       |   .   |   */
+/*    | SKULL | NOSE  |   */
+/*    |       |   .   |   */
+/*    |       |_______|   */
+/*    |       |           */
+/*    |_______|           */
+/*                        */
+/*   '.' -> eyes          */
+/**************************/
 typedef struct snake_head
 {
-	vertex eye[2];
-	int eye_size;
-	block nose;
-	block skull;
-	int direction;
-	int rotate;
+	vertex eye[2]; /* Left,Right eye vertices */
+	int eye_size; /* Eye size in pixels */
+	block nose; /* NOSE block */
+	block skull; /* SKULL block */
+	int direction; /* Movement direction of head */
+	int rotate; /* Rotation status 1: to be rotated; 0: no rotation needed */
 }snake_head;
 
+
+/**************************/
+/*  Obstacle in 2D Plane  */
+/*   __________________   */
+/*  |                  |  */
+/*  |                  |  */
+/*  |       .(position)|  */
+/*  |                  |  */
+/*  |                  |  */
+/*  |__________________|  */
+/*                        */
+/**************************/
 typedef struct obstacle
 {
-	block obst;
-	vertex position;
+	block obst; /* Block of obstacle */
+	vertex position; /* Origin position of obstacle in 2D Plane */
 }obstacle;
 
 #endif
