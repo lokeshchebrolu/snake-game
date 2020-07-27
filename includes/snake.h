fe2a173857aa97ef4a4e11a67fde3def1c990d8f
Lokesh Chebrolu <60533078+lokeshch007@users.noreply.github.com>
2020-07-27 14:36:18 +0530

    Added comments in snake.h
    Added comments for more understanding

--- includes/snake.h
+++ includes/snake.h
@@ -1,10 +1,13 @@
 #ifndef __SNAKE_H
 #define __SNAKE_H
+/* File Description: snake variables,values,function declarations*/
 
-/* Header file inclusion */
+/*** Header file inclusion ***/
+/* Game types file */
 #include"../includes/types.h"
 
-/* Symbols */
+/*** Symbols ***/
+/* Snake movement directions */
 #define UP 1
 #define DOWN 2
 #define LEFT 3
@@ -12,20 +15,35 @@
 #define END -1
 
 
-/* Variables */
+/*** Variables ***/
+/* Speed of snake in no.of co-ordinates it
+   will be added for next position */
 extern float snake_speed;
+
+/* Snake body block count excluding head */
 extern unsigned int snake_length;
 
+/* Snake head variable */
 extern snake_head head;
+
+/* Snake body pointer 
+   A linkedlist shall be used to implement body blocks*/
 extern snake_body *body;
 
+/* Egg variable */
 extern obstacle egg;
+
+/* Egg size variable */
 extern float egg_size;
+
+/* Egg hit status variable */
 extern int egg_hit;
 
+/* Snake dead status */
 extern int dead;
 
-/* Global function declarations */
+/*** Global function declarations ***/
+/* Snake data initialization function */
 extern void snake_init(void);
 #endif
 
